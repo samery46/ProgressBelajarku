8cca95ff9b3f4bf50c020a4eedb9b07d384343b4
Muhammad Syamsul Arifin <50190762+samery46@users.noreply.github.com>
2021-10-22 15:57:05 +0700

    Update trait_conflict_insteadof.php

--- Advance OOP PHP/trait_conflict_insteadof.php
+++ Advance OOP PHP/trait_conflict_insteadof.php
@@ -13,11 +13,13 @@ trait LowWatt{
 
 class SmartTV{
   use SmartElectronic, LowWatt {
-    SmartElectronic::efisiensi insteadof LowWatt // memanggil methode efisiensi mili SmartElectronic
+    SmartElectronic::efisiensi insteadof LowWatt; // memanggil methode efisiensi milik SmartElectronic
+    LowWatt:: efisiensi as efisiensiLow; // agar bisa diakses ,memberi nama baru/alias methode efisiensi milik trait LowWatt
   }
 }
 
 $produk01 = new SmartTV;
 echo $produk01->efisiensi(); // konsumsi daya 1.1
-
+echo "<br>";
+echo $produksi01->efisiensiLow(); // konsumsi daya 0.8
 ?>
