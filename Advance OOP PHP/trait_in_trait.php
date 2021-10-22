e2522781cb1e939414912ba82aeac73c1fe83eb8
Muhammad Syamsul Arifin <50190762+samery46@users.noreply.github.com>
2021-10-22 16:23:10 +0700

    Update trait_in_trait.php

--- Advance OOP PHP/trait_in_trait.php
+++ Advance OOP PHP/trait_in_trait.php
@@ -1,5 +1,7 @@
 <?php
 trait SmartElectronic{
+  public $internet = "Telkom Indihome"; // trait boleh terdapat property
+  public static $type = "Langganan"; // static property
   public function cekOS(){
     return "Android 9.0 (Pie)";
   }
@@ -17,4 +19,9 @@ class SmartTV{
 }
 
 $produk01 = new SmartTV;
+echo $produk01->internet; // Telkom Indihome
+echo "<br>";
+echo $produk01::type; // Langganan
+echo "<br>";
 echo $produk01->efisiensi(); // Android 9.0 (Pie) Konsumsi daya 0.8
+?>
